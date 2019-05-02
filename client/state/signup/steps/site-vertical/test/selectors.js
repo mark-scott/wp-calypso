5094d4075335448b586842267032a6155798f3eb
Jeff Bowen <jeff@automattic.com>
2019-05-02 12:49:58 -0400

    Test coverage for selectors

--- client/state/signup/steps/site-vertical/test/selectors.js
+++ client/state/signup/steps/site-vertical/test/selectors.js
@@ -1,6 +1,11 @@
 /** @format */
 
 /**
+ * External dependencies
+ */
+import { omit } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import {
@@ -9,8 +14,12 @@ import {
 	getSiteVerticalSlug,
 	getSiteVerticalIsUserInput,
 	getSiteVerticalPreview,
+	getSiteVerticalPreviewIframeContent,
+	getSiteVerticalPreviewLastShown,
+	getSiteVerticalPreviewTagline,
 	getSiteVerticalParentId,
 } from '../selectors';
+import { translate } from 'i18n-calypso';
 
 describe( 'selectors', () => {
 	const verticals = {
@@ -21,7 +30,13 @@ describe( 'selectors', () => {
 
 	const state = {
 		signup: {
+			siteMockupShown: 12345678,
 			steps: {
+				siteInformation: {
+					address: '1604 Wherever Ln',
+					phone: '555-4444',
+					title: 'Whatever you want it to be',
+				},
 				siteVertical: {
 					id: 'p4u',
 					name: 'felice',
@@ -90,6 +105,53 @@ describe( 'selectors', () => {
 			expect( getSiteVerticalParentId( state ) ).toEqual(
 				state.signup.steps.siteVertical.parentId
 			);
+		} );
+	} );
+	describe( 'getSiteVerticalPreviewLastShown', () => {
+		test( 'should return on null on empty', () => {
+			expect( getSiteVerticalPreviewLastShown() ).toBe( null );
+		} );
+		test( 'should return on null on null', () => {
+			expect( getSiteVerticalPreviewLastShown( omit( state, 'signup.siteMockupShown' ) ) ).toBe(
+				null
+			);
+		} );
+		test( 'should return signup.siteMockupShown value', () => {
+			expect( getSiteVerticalPreviewLastShown( state ) ).toBe( 12345678 );
+		} );
+	} );
+	describe( 'getSiteVerticalPreviewTagline', () => {
+		test( 'should return default value on empty', () => {
+			expect( getSiteVerticalPreviewTagline() ).toEqual(
+				translate( 'You’ll be able to customize this to your needs.' )
+			);
+		} );
+		test( 'should return address with address and no phone', () => {
+			expect(
+				getSiteVerticalPreviewTagline( omit( state, 'signup.steps.siteInformation.phone' ) )
+			).toEqual( '1604 Wherever Ln' );
+		} );
+		test( 'should return phone with phone and no address', () => {
+			expect(
+				getSiteVerticalPreviewTagline( omit( state, 'signup.steps.siteInformation.address' ) )
+			).toEqual( '555-4444' );
+		} );
+		test( 'should return both with address & phone', () => {
+			expect( getSiteVerticalPreviewTagline( state ) ).toEqual(
+				'1604 Wherever Ln &middot; 555-4444'
+			);
+		} );
+	} );
+	describe( 'getSiteVerticalPreviewIframeContent', () => {
+		test( 'should return empty object on empty', () => {
+			expect( getSiteVerticalPreviewIframeContent() ).toEqual( {} );
+		} );
+		test( 'should return populated object on valid state', () => {
+			expect( getSiteVerticalPreviewIframeContent( state ) ).toEqual( {
+				body: '<!--gutenberg-besties-forever <p>Fist bump!</p>-->',
+				tagline: '1604 Wherever Ln &middot; 555-4444',
+				title: 'Whatever you want it to be',
+			} );
 		} );
 	} );
 } );
